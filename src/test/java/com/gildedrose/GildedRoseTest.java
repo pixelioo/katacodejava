4e5fc0b5b0475d434d60866b62ff2e1042c48940
Tuncer Kocek <tuncerkocek@gmail.com>
2020-07-31 18:16:58 +0200

    adding new unit test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,56 +8,209 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void isFoo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
+    void sellinOfConjuredAfter2Day() {
+        Item[] items = new Item[]{new Item("Conjured Mana Cake", 3, 6)};
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.getItem(0).getName());
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
+        assertEquals(1, app.getItem(0).getSellIn());
     }
 
     @Test
-    void isNotFoo() {
-        Item[] items = new Item[]{new Item("tomatoes", 0, 0)};
+    void qualityOfConjuredAfter2Day() {
+        Item[] items = new Item[]{new Item("Conjured Mana Cake", 3, 6)};
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        Assertions.assertNotEquals("foo", app.getItem(0).getName());
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
+        assertEquals(4, app.getItem(0).getQuality());
     }
 
     @Test
-    void isTomatoes() {
-        Item[] items = new Item[]{new Item("tomatoes", 0, 0)};
+    void qualityOfConjuredAfter3Day() {
+        Item[] items = new Item[]{new Item("Conjured Mana Cake", 5, 8)};
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("tomatoes", app.getItem(0).getName());
+        for (int i = 0; i < 3; i++) {
+            app.updateQuality();
+        }
+        assertEquals(5, app.getItem(0).getQuality());
+    }
+
+    @Test
+    void sellinOfConjuredAfter3Day() {
+        Item[] items = new Item[]{new Item("Conjured Mana Cake", 5, 8)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 3; i++) {
+            app.updateQuality();
+        }
+        assertEquals(2, app.getItem(0).getSellIn());
+    }
+
+    @Test
+    void sellinOfSulfurasAfter2Day() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 15, 80)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
+        assertEquals(15, app.getItem(0).getSellIn());
+    }
+
+    @Test
+    void qualityOfSulfurasAfter2Day() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 15, 80)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
+        assertEquals(80, app.getItem(0).getQuality());
+    }
+
+    @Test
+    void qualityOfSulfurasAfter3Day() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 25, 80)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 3; i++) {
+            app.updateQuality();
+        }
+        assertEquals(80, app.getItem(0).getQuality());
+    }
+
+      @Test
+    void sellinOfSulfurasAfter3Day() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 25, 80)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 3; i++) {
+            app.updateQuality();
+        }
+        assertEquals(25, app.getItem(0).getSellIn());
     }
     
+    
+    
     @Test
-    void canWeSellTomatoes() {
-        Item[] items = new Item[]{new Item("tomatoes", 10, 10)};
+    void sellinOfAgedAfter2Day() {
+        Item[] items = new Item[]{new Item("Aged Brie",  2, 0)};
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(10,app.getItem(0).getSellIn());
-    }
-     @Test
-    void cannotSellTomatoes() {
-        Item[] items = new Item[]{new Item("tomatoes", 0, 10)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
         assertEquals(0, app.getItem(0).getSellIn());
     }
+
+    @Test
+    void qualityOfAgedsAfter2Day() {
+        Item[] items = new Item[]{new Item("Aged Brie",  2, 0)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
+        assertEquals(2, app.getItem(0).getQuality());
+    }
+
+    @Test
+    void qualityOfAgedsAfter3Day() {
+        Item[] items = new Item[]{new Item("Aged Brie", 2, 0)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 3; i++) {
+            app.updateQuality();
+        }
+        assertEquals(4, app.getItem(0).getQuality());
+    }
+
+      @Test
+    void sellinOfAgedAfter3Day() {
+        Item[] items = new Item[]{new Item("Aged Brie", 2, 0)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 3; i++) {
+            app.updateQuality();
+        }
+        assertEquals(-1, app.getItem(0).getSellIn());
+    }
+    
+    
+    
+       
+    @Test
+    void sellinOfBackstageAfter2Day() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",  14, 21)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
+        assertEquals(12, app.getItem(0).getSellIn());
+    }
+
+    @Test
+    void qualityOfBackstageAfter2Day() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",  14, 21)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
+        assertEquals(23, app.getItem(0).getQuality());
+    }
+
+    @Test
+    void qualityOfBackstageAfter3Day() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 3; i++) {
+            app.updateQuality();
+        }
+        assertEquals(24, app.getItem(0).getQuality());
+    }
+
+      @Test
+    void sellinOfBackstageAfter3Day() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 3; i++) {
+            app.updateQuality();
+        }
+        assertEquals(11, app.getItem(0).getSellIn());
+    }
+    
+    
     
     @Test
-    void isTomatoesGoodQuality() {
-        Item[] items = new Item[]{new Item("tomatoes", 10, 10)};
+    void sellinOfOtherAfter2Day() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 4, 14)};
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(10, app.getItem(0).getQuality());
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
+        assertEquals(2, app.getItem(0).getSellIn());
     }
-      @Test
-    void isTomatoesBadQuality() {
-        Item[] items = new Item[]{new Item("tomatoes", 10, 10)};
+
+    @Test
+    void qualityOfOtherAfter2Day() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose",  4, 14)};
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(0, app.getItem(0).getQuality());
+        for (int i = 0; i < 2; i++) {
+            app.updateQuality();
+        }
+        assertEquals(12, app.getItem(0).getQuality());
+    }
+
+    @Test
+    void qualityOfOtherAfter3Day() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose",4, 14)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 3; i++) {
+            app.updateQuality();
+        }
+        assertEquals(11, app.getItem(0).getQuality());
+    }
+
+      @Test
+    void sellinOfOtherAfter3Day() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 4, 14)};
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 3; i++) {
+            app.updateQuality();
+        }
+        assertEquals(1, app.getItem(0).getSellIn());
     }
 }
