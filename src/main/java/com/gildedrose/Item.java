ea8236911351045fa099321480a69261bcb06cd5
Tuncer Kocek <tuncerkocek@gmail.com>
2020-07-31 16:41:24 +0200

    add setter and getter

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -2,11 +2,11 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
+    private String name;
 
-    public int sellIn;
+    private int sellIn;
 
-    public int quality;
+    private int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -16,6 +16,48 @@ public class Item {
 
    @Override
    public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+        return this.getName() + ", " + this.getSellIn() + ", " + this.getQuality();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the sellIn
+     */
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    /**
+     * @param sellIn the sellIn to set
+     */
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    /**
+     * @return the quality
+     */
+    public int getQuality() {
+        return quality;
+    }
+
+    /**
+     * @param quality the quality to set
+     */
+    public void setQuality(int quality) {
+        this.quality = quality;
     }
 }
