21bce24ea9dffd75cdc9b1e2101abf7951e153ad
Tuncer Kocek <tuncerkocek@gmail.com>
2020-07-31 14:08:56 +0200

    adding new testcase

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +8,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void isFoo() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void isNotFoo() {
+        Item[] items = new Item[]{new Item("tomatoes", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        Assertions.assertNotEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void isTomatoes() {
+        Item[] items = new Item[]{new Item("tomatoes", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("tomatoes", app.items[0].name);
+    }
+    
+    @Test
+    void canWeSellTomatoes() {
+        Item[] items = new Item[]{new Item("tomatoes", 10, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(10, app.items[0].sellIn);
+    }
+     @Test
+    void cannotSellTomatoes() {
+        Item[] items = new Item[]{new Item("tomatoes", 0, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].sellIn);
+    }
 }
